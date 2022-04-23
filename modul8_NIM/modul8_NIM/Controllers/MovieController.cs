9db9f4f105a841f3def334a4fe25af2356e3d3be
agistaputri20 <agistaputri@student.telkomuniversity.ac.id>
2022-04-23 17:53:12 +0700

    mod8

---
+++ modul8_NIM/modul8_NIM/Controllers/MovieController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul8_1302201140.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        // GET: api/<MovieController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            String jsonString = File.ReadAllText("E:/kuliah semester 4/KPL (Kontruksi Perangkat Lunak)/Praktikum/JurnalMod8/modul8_NIM/modul8_NIM/Movie.json");
+            return new string[] {};
+        }
+
+        // GET api/<MovieController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MovieController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MovieController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MovieController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- modul8_NIM/modul8_NIM/Movie.cs
+++ modul8_NIM/modul8_NIM/Movie.cs
@@ -1,6 +1,31 @@
-﻿namespace modul8_1302201140
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace modul8_1302201140
 {
-    public class Class
+    public class Movie
     {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie(string title, string director, List<string> stars, string description)
+        {
+            Title = title;
+            Director = director;
+            Stars = stars;
+            Description = description;
+
+
+        }
+
+        
     }
 }
